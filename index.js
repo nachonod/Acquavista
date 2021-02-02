99c156c3b1d8d123cef6cf4ddd77c57d099e5f82
nachonod <nachominolli@gmail.com>
2021-02-02 10:02:50 -0300

    web ready

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var modeIndex = 0;
 container = document.querySelector("#container");
 aerea = document.querySelector("#aerea");
 aereaDos = document.querySelector("#aereados");
+aereaTres = document.querySelector("#aereatres");
+infospotList = document.querySelector("#infospotlist");
 
 panorama = new PANOLENS.ImagePanorama("./assets/GALP_Acquavista_360.jpg");
 
@@ -16,35 +18,26 @@ viewer.add(panorama);
 infospot = new PANOLENS.Infospot(110, PANOLENS.DataImage.Info);
 infospot.position.set(1500, 170, -1500);
 infospot.addHoverElement(aerea, 0);
-infospot.addEventListener("click", function () {
-  this.focus();
-});
-
 panorama.add(infospot);
 
 // Info Point Aerea 2
 infospot = new PANOLENS.Infospot(100, PANOLENS.DataImage.Info);
 infospot.position.set(100, 170, -1500);
 infospot.addHoverElement(aereaDos, 0);
-infospot.addEventListener("click", function () {
-  this.focus();
-});
+panorama.add(infospot);
 
+// Info Point Aerea 3
+infospot = new PANOLENS.Infospot(150, PANOLENS.DataImage.Info);
+infospot.position.set(-1800, 170, -1500);
+infospot.addHoverElement(aereaTres, 0);
 panorama.add(infospot);
 
 // Info Point Texto
 
-// infospot = new PANOLENS.Infospot(40, PANOLENS.DataImage.Info);
-// infospot.position.set(200, -55, 510);
-// infospot.addHoverText(
-//   "Sabias que esta laguna tiene la superficien de 20,8 Piscinas Olimpicas",
-//   -50
-// );
-// infospot.addEventListener("click", function () {
-//   this.focus();
-// });
-
-// panorama.add(infospot);
+infospot = new PANOLENS.Infospot(40, PANOLENS.DataImage.Info);
+infospot.position.set(200, -55, 510);
+infospot.addHoverElement(infospotList, 0);
+panorama.add(infospot);
 
 // Orbit Controls
 // Momentum
